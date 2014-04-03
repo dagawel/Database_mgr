d7c788d066253a804fd0d37a2c00563983eb2406
dagawel <dagmara.gawel@gmail.com>
2014-04-03 16:45:13 +0200

    OrientDB class added

--- DocumentDatabase/src/documentdatabase/OrientDB.java
+++ DocumentDatabase/src/documentdatabase/OrientDB.java
@@ -16,13 +16,39 @@ public class OrientDB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ODatabaseDocumentTx db = new ODatabaseDocumentTx("local:/temp/test");
-        db.open("admin", "admin");
+        ////////////////////// 1 /////////////////////////
 
-        try {
-          // YOUR CODE
-        } finally {
-          db.close();
-        }
+        ////////////////////// 2 /////////////////////////
+//        ODatabaseDocumentTx db = new ODatabaseDocumentTx("local:/tmp/db/scratchpad");
+//        db.create(); 
+//        ODocument doc = db.newInstance(); 
+//        doc.field("name", "Dagmara");  
+//        doc.field("surname", "Gaweł"); 
+//        doc.save();
+//        db.close(); 
+        
+        ////////////////////// 2 /////////////////////////
+        // OPEN THE DATABASE
+        ODatabaseDocumentTx db = new ODatabaseDocumentTx("remote:localhost/petshop").open("admin", "admin");
+
+        // CREATE A NEW DOCUMENT AND FILL IT
+        ODocument doc = new ODocument("Person");
+        doc.field( "name", "Luke" );
+        doc.field( "surname", "Skywalker" );
+        doc.field( "city", new ODocument("City").field("name","Rome").field("country", "Italy") );
+
+        // SAVE THE DOCUMENT
+        doc.save();
+        db.close();
+        
+        // USE THE DATABASE
+//        ODatabaseDocumentTx db = new ODatabaseDocumentTx("local:/temp/test");
+//        db.open("admin", "admin");
+//
+//        try {
+//          // YOUR CODE
+//        } finally {
+//          db.close();
+//        }
     }
 }
